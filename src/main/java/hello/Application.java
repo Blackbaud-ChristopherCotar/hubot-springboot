0296c8086ea61ff313bbb02b587d070a31ce2eb7
Chris Cotar <christopher.cotar@blackbaud.com>
2015-02-25 23:51:57 -0600

    some socket stuff

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -1,16 +1,43 @@
 package hello;
 
-import java.util.Arrays;
-
+import org.eclipse.jetty.websocket.client.ClientUpgradeRequest;
+import org.eclipse.jetty.websocket.client.WebSocketClient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+
+import java.net.URI;
+import java.util.Arrays;
+import java.util.concurrent.TimeUnit;
 
 @SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
         ApplicationContext ctx = SpringApplication.run(Application.class, args);
+
+        String destUri = "ws://echo.websocket.org";
+        if (args.length > 0) {
+            destUri = args[0];
+        }
+        WebSocketClient client = new WebSocketClient();
+        SimpleEchoSocket socket = new SimpleEchoSocket();
+        try {
+            client.start();
+            URI echoUri = new URI(destUri);
+            ClientUpgradeRequest request = new ClientUpgradeRequest();
+            client.connect(socket, echoUri, request);
+            System.out.printf("Connecting to : %s%n", echoUri);
+            socket.awaitClose(5, TimeUnit.SECONDS);
+        } catch (Throwable t) {
+            t.printStackTrace();
+        } finally {
+            try {
+                client.stop();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
 
         System.out.println("Let's inspect the beans provided by Spring Boot:");
 
