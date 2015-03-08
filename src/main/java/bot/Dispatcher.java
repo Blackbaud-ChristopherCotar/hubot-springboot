55fd4967c67ebb23bca8268de9fd4392fd559b69
Chris Cotar <christopher.cotar@blackbaud.com>
2015-03-07 18:23:44 -0600

    did some refactoring, still not where I want it to be though

--- src/main/java/bot/Dispatcher.java
+++ src/main/java/bot/Dispatcher.java
@@ -1,24 +1,84 @@
 package bot;
 
 import bot.entities.SlackChatMessage;
+import bot.entities.SlackRTMResponse;
 import bot.handlers.Ping;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * Evaluates commands and calls the correct handler
  */
 public class Dispatcher {
 
-    public Dispatcher(){}
+    private static final String SLACK_TYPE_MESSAGE = "message";
+
+    private int idCounter = 0;
+    private SlackRTMResponse roomState;
+
+    public Dispatcher(SlackRTMResponse slackRTMResponse){
+        this.roomState = slackRTMResponse;
+    }
 
     public String respond(SlackChatMessage slackChatMessage) {
-        String message = slackChatMessage.getText();
+        System.out.println("responding...");
+        if(isBotCommand(slackChatMessage)) {
+            String message = slackChatMessage.getText();
+            String command = extractBotCommand(message);
 
-        Ping ping = new Ping();
-        if(ping.handlesCommand(message)) {
-            return ping.processCommand(slackChatMessage);
+            Ping ping = new Ping();
+            if(ping.handlesCommand(command)) {
+                String handlerMessage = ping.processCommand(slackChatMessage);
+                return processValidChatResponse(slackChatMessage, handlerMessage);
+            }
         }
 
-        // no handler processes this command
-        return "I can't process your command!";
+        // no handler processes this command, is null a could return value? meh
+        return null;
+    }
+
+    String processValidChatResponse(SlackChatMessage slackChatMessage, String handlerMessage) {
+        ObjectMapper mapper = new ObjectMapper();
+
+        SlackChatMessage response = new SlackChatMessage();
+        response.setId(++idCounter);
+        response.setChannel(slackChatMessage.getChannel());
+        response.setText(handlerMessage);
+        response.setType(SLACK_TYPE_MESSAGE);
+
+        String responseText = "";
+        try {
+            responseText =  mapper.writeValueAsString(response);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+
+        return responseText;
+    }
+
+    String extractBotCommand(String text) {
+        String botName = roomState.getSelf().getName();
+        String botId = roomState.getSelf().getId();
+
+        Pattern p = Pattern.compile("^\\s*(" + botName + "|<@" + botId +  ">:?)(.*)$", Pattern.CASE_INSENSITIVE);
+        Matcher m = p.matcher(text);
+        if(m.matches()) {
+            return m.group(2).trim();
+        }
+        return null;
+    }
+
+    boolean isBotCommand(SlackChatMessage chatMessage) {
+        if(chatMessage.getType() != null
+                && chatMessage.getType().equals(SLACK_TYPE_MESSAGE)
+                && chatMessage.getText() != null
+                && extractBotCommand(chatMessage.getText()) != null) {
+            return true;
+        }
+
+        return false;
     }
 }
--- src/main/java/bot/EventSocket.java
+++ src/main/java/bot/EventSocket.java
@@ -23,29 +23,20 @@ import org.springframework.beans.factory.annotation.Autowired;
 public class EventSocket {
 
     private final CountDownLatch closeLatch;
-    private int idCounter = 0;
-    private String botName = "";
-    private String botUserId = "";
 
     private static final String SLACK_TYPE_MESSAGE = "message";
 
     @SuppressWarnings("unused")
     private Session session;
-    private SlackRTMResponse state;
+    private Dispatcher dispatcher;
 
     @Autowired
     public EventSocket() {
         this.closeLatch = new CountDownLatch(1);
     }
 
-    public void setState(SlackRTMResponse state) {
-        this.state = state;
-        this.botName = state.getSelf().getName();
-        this.botUserId = state.getSelf().getId();
-    }
-
-    public boolean awaitClose(int duration, TimeUnit unit) throws InterruptedException {
-        return this.closeLatch.await(duration, unit);
+    public void setState(SlackRTMResponse roomState) {
+        this.dispatcher = new Dispatcher(roomState);
     }
 
     @OnWebSocketClose
@@ -57,50 +48,34 @@ public class EventSocket {
 
     @OnWebSocketConnect
     public void onConnect(Session session) {
-        System.out.printf("Got connect: %s%n", session);
+        System.out.printf("Got connection: %s%n", session);
         this.session = session;
     }
 
     @OnWebSocketMessage
     public void onMessage(String msg) {
-        ObjectMapper mapper = new ObjectMapper();
-        Dispatcher dispatcher = new Dispatcher();
-
-        try {
-            SlackChatMessage chatMessage = mapper.readValue(msg, SlackChatMessage.class);
-            if(isBotCommand(chatMessage)) {
-                String command = extractBotCommand(chatMessage.getText());
-                chatMessage.setText(command);
-                SlackChatMessage response = new SlackChatMessage();
-                response.setId(++idCounter);
-                response.setChannel(chatMessage.getChannel());
-                response.setText(dispatcher.respond(chatMessage));
-                response.setType(SLACK_TYPE_MESSAGE);
-                session.getRemote().sendString(mapper.writeValueAsString(response));
+        SlackChatMessage chatMessage = parseMsg(msg);
+        String chatResponse = dispatcher.respond(chatMessage);
+        if(chatResponse != null) {
+            try {
+                session.getRemote().sendString(chatResponse);
+            } catch (IOException e) {
+                e.printStackTrace();
             }
-        } catch (IOException e) {
-            e.printStackTrace();
         }
         System.out.printf("Got msg: %s%n", msg);
     }
 
-    public boolean isBotCommand(SlackChatMessage chatMessage) {
-        if(chatMessage.getType() != null
-                && chatMessage.getType().equals(SLACK_TYPE_MESSAGE)
-                && chatMessage.getText() != null
-                && extractBotCommand(chatMessage.getText()) != null) {
-            return true;
+    SlackChatMessage parseMsg(String message) {
+        ObjectMapper mapper = new ObjectMapper();
+        SlackChatMessage slackChatMessage = null;
+        try {
+            slackChatMessage = mapper.readValue(message, SlackChatMessage.class);
+        } catch (IOException e) {
+            e.printStackTrace();
         }
 
-        return false;
+        return slackChatMessage;
     }
 
-    public String extractBotCommand(String text) {
-        Pattern p = Pattern.compile("^\\s*(" + botName + "|<@" + botUserId +  ">:?)(.*)$", Pattern.CASE_INSENSITIVE);
-        Matcher m = p.matcher(text);
-        if(m.matches()) {
-            return m.group(2).trim();
-        }
-        return null;
-    }
 }
