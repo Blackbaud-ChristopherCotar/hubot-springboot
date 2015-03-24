123b730b39f210cf553a114e376d0e81f0e3b826
Chris Cotar <christopher.cotar@blackbaud.com>
2015-03-23 22:55:58 -0500

    enable CORS for requests and create basic reactJS display of metrics JSON response

--- src/main/resources/static/js/dashboard.js
+++ src/main/resources/static/js/dashboard.js
@@ -1,12 +1,31 @@
-(function() {
-    var Hello = React.createClass({displayName: 'Hello',
-       render: function() {
-           return React.createElement("div", null, "Hello ", this.props.name);
-       }
+$('document').ready(function(){
+    var Metrics = React.createClass({
+        loadMetrics: function() {
+            $.ajax({
+                url: 'http://localhost:8001/metrics',
+                dataType: 'json',
+                success: function(data) {
+                    this.setState({data: JSON.stringify(data)});
+                    console.log(JSON.stringify(data));
+                }.bind(this),
+                error: function(xhr, status, err) {
+                    console.error(this.props.url, status, err.toString());
+                }.bind(this)
+            });
+        },
+        getInitialState: function() {
+            return {data: {}};
+        },
+        componentDidMount: function() {
+            this.loadMetrics();
+        },
+        render: function() {
+            return React.createElement("div", null, this.state.data);
+        }
     });
 
     React.render(
-       React.createElement(Hello, {name: "World"}),
+       React.createElement(Metrics, null),
        document.getElementById('container')
     );
-})();
+});
